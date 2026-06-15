6c50799f9f2ec8181ed631382781e8f8f2d5c3b5
Vista <nguyenvan.nha016@gmail.com>
2026-06-15 16:46:03 +0700

    Initial Commit

---
+++ Basic Expense Tracker/Basic_Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Expense_TrackerApp.swift
+//  Basic Expense Tracker
+//
+//  Created by Nguyen Nha on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Basic_Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Expense TrackerUITests/Basic_Expense_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Basic_Expense_TrackerUITestsLaunchTests.swift
+//  Basic Expense TrackerUITests
+//
+//  Created by Nguyen Nha on 6/15/26.
+//
+
+import XCTest
+
+final class Basic_Expense_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
